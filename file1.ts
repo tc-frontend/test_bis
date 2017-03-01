fd8a7d4ed5056a86c667cc7789ec60219e3322dc
tc-frontend <roberto.loreto@avanade.com>
2017-03-01 20:46:23 +0100

    +Commit 3

--- file1.ts
+++ file1.ts
@@ -1,12 +1,15 @@
 export class Test1 {
 
     private text1: string;
+    private text2: string;
 
     constructor(){
-        this.text1 = 'Test';
+        this.text1 = 'Test1';
+        this.text1 = 'Test2';
     }
 
     foo123456789():void{
 
     }
+    
 }
